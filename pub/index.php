40bf30fdb6bd2c2a5c17f28c39a35197acf1fadf
Slava <viacheslav.mail@gmail.com>
2023-05-05 13:56:45 +0300

    Update index.php

--- pub/index.php
+++ pub/index.php
@@ -6,8 +6,5 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-ini_set('upload_max_filesize', '50M');
-ini_set('post_max_size', '50M');
-
 $instance = new App\Controller\Index();
 $instance->execute();
